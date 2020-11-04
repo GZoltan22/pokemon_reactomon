ab5253e7aee1ba3965b3f6f4a868a1665eb7d58c
GZoltan22 <55540615+GZoltan22@users.noreply.github.com>
2020-11-04 18:56:20 +0100

    PokemonItem is working with Hooks

--- src/components/PokemonItem.js
+++ src/components/PokemonItem.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-export default class PokemonItem extends Component {
+const PokemonItem = props => {
     
-    render() {
-        const {url, name } = this.props.pokemon;
+        const {url, name } = props.pokemon;
         const id = url.substring(url.lastIndexOf('/') - 1, url.lastIndexOf('/'));
         return (
-            <div className="card" onClick={this.props.detailPokemon.bind(this, url)}>
+            <div className="card" onClick={props.detailPokemon.bind(this, url)}>
                 <Link to={`Pokemon/${id}`}>
                     { name }
                 </Link>
             </div>
         )
-    }
 }
 
 // PropTypes
 PokemonItem.propTypes = {
     pokemons: PropTypes.object.isRequired
 }
+
+export default PokemonItem
--- src/components/Pokemons.js
+++ src/components/Pokemons.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 import PokemonItem from './PokemonItem';
 
 import PropTypes from 'prop-types';
