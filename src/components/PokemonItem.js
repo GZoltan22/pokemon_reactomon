4d35cc752b00adab830b742db244a33ef158afe7
GZoltan22 <55540615+GZoltan22@users.noreply.github.com>
2020-11-09 11:11:05 +0100

    pokemon pictures are already shown

--- src/components/PokemonItem.js
+++ src/components/PokemonItem.js
@@ -17,10 +17,17 @@ font-weight: bold;
 
 
 const PokemonItem = props => {
-    const [pokemonPic, setPokemonPic] = useState([]);
+    const [pokemonPic, setPokemonPic] = useState("");
 
     const {url, name } = props.pokemon;
     const id = url.substring(url.lastIndexOf('/') - 1, url.lastIndexOf('/'));
+    // const regId = "/[0-9]+/(|$)";
+    const last = url.split('&').pop().split('=');
+    const pokemonId = last[0];
+    const pokId = url.split("/")
+    console.log(pokId[6])
+    console.log("under there is a pokemonId")
+    console.log(pokemonId);
 
 
 
@@ -28,16 +35,20 @@ const PokemonItem = props => {
 
     useEffect(() => {
         console.log("it starts")
-        axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-        .then(res => setPokemonPic({pokemonPic: res.data.sprites.other.["official-artwork"].front_default}))
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${pokId[6]}/`)
+        .then((response) => {
+            setPokemonPic(response.data.sprites.other["official-artwork"].front_default);
+        })
+        // .then(res => setPokemonPic({pokemonPic: res.data.sprites.other.["official-artwork"].front_default}))
         console.log(pokemonPic)
         console.log("asd")
-      },[])
+      },[pokemonPic])
 
     return (
         <Link to={`Pokemon/${id}`}>
         {/* <ThemeProvider theme={theme}> */}
             <Div className="card" onClick={props.detailPokemon.bind(this, url)}>
+                <img src = {pokemonPic} alt="Pokemons Pictures" width="80"></img>
                 <p>
                     { name }
                 </p>
